b3e0432ce959677e852397b73a99be87cbba4bbd
s24293 <s24293@pjwstk.edu.pl>
2024-06-20 16:16:24 +0200

    fix

--- cmake-build-debug/vendored/SDL/include/SDL2/SDL_revision.h
+++ cmake-build-debug/vendored/SDL/include/SDL2/SDL_revision.h
@@ -2,7 +2,7 @@
 #define SDL_REVISION_NUMBER 0
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-2.30.2-g2fb36d9 (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL-2.30.2-g582c8f8 (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL-2.30.2-g2fb36d9"
+#define SDL_REVISION "SDL-2.30.2-g582c8f8"
 #endif
